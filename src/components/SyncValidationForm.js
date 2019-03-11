ceac596e7739c93a6ba593d174d86a43414f2ce5
ru03 <ruben.lmangas@gmail.com>
2019-03-11 20:18:26 +0100

    add selector to get value from state in form

--- src/components/SyncValidationForm.js
+++ src/components/SyncValidationForm.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { Field, FormSection, reduxForm } from 'redux-form'
+import { Field, FormSection, reduxForm, formValueSelector } from 'redux-form'
+import { connect } from 'react-redux';
 import Input from './UI/Input/Input';
 import Checkbox from './UI/Checkbox/Checkbox';
 import FormButtons from './UI/FormButtons/FormButtons'
 import { syncValidators } from './../helpers/formValidation';
 
 const SyncValidationForm = (props) => {
-    const { checkOpts, handleSubmit, reset, submitting, pristine } = props;
+    const { checkOpts, handleSubmit, reset, submitting, pristine, hasHobbie } = props;
+    console.log(hasHobbie)
     return (
         <div className="columns">
             <div className="column is-half is-offset-one-quarter">
@@ -14,21 +16,31 @@ const SyncValidationForm = (props) => {
                     <Field name="email" label="Email" component={Input} type="email" />
                     <Field name="name" label="Name" component={Input} type="text" />
                     <Field name="age" label="Age" component={Input} type="text" />
-                    <FormSection name="hobbies">
-                        <div className="field is-grouped is-grouped-centered">
-                            {
-                                checkOpts.map((data, i) =>
-                                    <Field id={data}
-                                        key={data}
-                                        name={data}
-                                        label={data}
-                                        type="checkbox"
-                                        component={Checkbox}
-                                    />
-                                )
-                            }
-                        </div>
-                    </FormSection>
+                    <div className="field">
+                        <Field id='hasHobbies'
+                            name="hasHobbie"
+                            label="Has hobbies?"
+                            type="checkbox"
+                            component={Checkbox}
+                        />
+                    </div>
+                    {
+                        hasHobbie && <FormSection name="hobbies">
+                            <div className="field is-grouped is-grouped-centered">
+                                {
+                                    checkOpts.map((data, i) =>
+                                        <Field id={data}
+                                            key={data}
+                                            name={data}
+                                            label={data}
+                                            type="checkbox"
+                                            component={Checkbox}
+                                        />
+                                    )
+                                }
+                            </div>
+                        </FormSection>
+                    }
                     <FormButtons pristine={pristine} submittingHandle={submitting} reset={() => reset()} />
                 </form>
             </div>
@@ -36,7 +48,14 @@ const SyncValidationForm = (props) => {
     )
 }
 
-export default reduxForm({
+const selector = formValueSelector('SyncValidationForm');
+const mapStateToProps = state => ({
+    hasHobbie: selector(state, 'hasHobbie'),
+});
+
+const syncValidationForm = reduxForm({
     form: 'SyncValidationForm',
     validate: syncValidators,
 })(SyncValidationForm);
+
+export default connect(mapStateToProps)(syncValidationForm);
